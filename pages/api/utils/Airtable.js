e310988ce9abe529f78989f274b2284b6f370dff
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-31 09:24:36 +0200

    added all necessary infos for future component

--- pages/api/utils/Airtable.js
+++ pages/api/utils/Airtable.js
@@ -12,7 +12,12 @@ const minifyRecords = (records) => {
 const getMinifiedRecord = (record) => {
   return {
     id: record.id,
+    // info: record.fields,
     name: record.fields.Produit,
+    weight: record.fields.Conditionnement,
+    price: record.fields["Prix d'achat unitaire €HT"],
+    pricePerKg: record.fields["Prix d'achat kg/g/L €HT"],
+    stock: record.fields.Dispo,
   };
 };
 

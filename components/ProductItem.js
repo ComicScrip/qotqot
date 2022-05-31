efda84d66fa64ea36e0567a238fdef3a5de318d8
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-31 16:12:50 +0200

    added more style

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -6,15 +6,24 @@ function ProductItem(props) {
     <div className={style.item_wrapper}>
       <div className={style.item_picture}> Image</div>
       <div className={style.item_detail}>
-        <div>{props.name}</div>
-        <div>{props.weight}</div>
+        <div className={style.item_title}>{props.name}</div>
+        <div className={style.item_weight}>{props.weight}</div>
       </div>
       <div className={style.price}>
-        <div className={style.itemPrice}>{props.price} HT</div>
-        <div className={style.itemPricePerKg}>{props.pricePerKg} HT/Kg</div>
+        <div className={style.itemPrice}>{props.price}€ HT</div>
+        <div className={style.itemPricePerKg}>{props.pricePerKg}€ HT /Kg</div>
       </div>
       <div className={style.item_stock}>
-        <div className={style.stock_display}>{props.stock}</div>
+        <div
+          className={
+            props.stock === "En rupture"
+              ? style.out_of_stock
+              : style.stock_display_instock
+          }
+        >
+          {props.stock}
+        </div>
+
         <div className={style.counter}>
           <div>-</div>
           <div>Qt</div>

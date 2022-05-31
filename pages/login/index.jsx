0d5442a0c1fe98cfe3ec0131bdef93ae8137f329
Anna Boval <anna.bo1106@gmail.com>
2022-05-31 12:05:17 +0200

    login page modified and more styled

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,38 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
-const index = () => {
+const login = () => {
   return (
-    <div className="login">
-      <div>
-        <Image
-          src="/../public/assets/Logo-QotQot-Sans-fond-carré_44rzy5xn.png"
-          alt="logo_qotqot"
-          width={100}
-          height={100}
-        />
+    <div id="login" className="w-full h-full m-auto bg-hero-sm ">
+      <div className="m-auto mt-16 flex flex-col justify-center items-center ">
+        <div>
+          <Image
+            src="/../public/assets/Logo-QotQot-Sans-fond-carré_44rzy5xn.png"
+            alt="logo_qotqot"
+            width={200}
+            height={200}
+          />
+        </div>
+        <h1 className="my-8 tracking-2">Espace Professionnel</h1>
       </div>
-      <h1>Espace Professionnel</h1>
-      <form action="/send-data-here" method="post">
-        <label>Email</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          required
-          minLength="8"
-          maxLength="50"
-        />
-        <label>Mot de passe :</label>
-        <input type="text" id="name" name="name" required />
+      <form
+        action="/send-data-here"
+        method="post"
+        className="flex flex-col px-3 py-4 "
+      >
+        <div className="text-[#7F7F7F] border-2 border-gray-300 h-14 px-4 flex flex-col rounded-lg">
+          <label className="text-[#7F7F7F]">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="text-[#7F7F7F]"
+            required
+            minLength="8"
+            maxLength="50"
+            placeholder="jean.dupont@mail.com"
+          />
+        </div>
+        <div className="text-[#7F7F7F] border-2 border-gray-300 my-3 h-14 px-4 flex flex-col rounded-lg">
+          <label className="text-[#7F7F7F] ">Mot de passe :</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            className="text-[#7F7F7F]"
+            required
+            minLength="8"
+            maxLength="15"
+            placeholder="votre mot de passe"
+          />
+        </div>
       </form>
-      <label>
-        <input type="checkbox" />
-        Se souvenir de moi
-      </label>
-      <button type="submit">Se connecter</button>
+      <div className="m-auto py-2 px-3">
+        <label className="text-[#7a7a7a]">
+          <input type="checkbox" className="mr-3 ml-1 border-gray-200" />
+          Se souvenir de moi
+        </label>
+      </div>
+      <div className="flex justify-center">
+        <button
+          className=" border-2 rounded-md px-32 py-5 uppercase text-sm text-white bg-[#339966] font-medium"
+          type="submit"
+        >
+          Se connecter
+        </button>
+      </div>
+      <div className="flex justify-center px-3">
+        <p className=" text-[#339966] underline underline-offset-1 py-2">
+          Mot de passe oublié ?
+        </p>
+      </div>
     </div>
   );
 };
 
-export default index;
+export default login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-sm": "url('/../public/assets/qotqot-bg.png')",
+      },
+    },
   },
   plugins: [],
 };

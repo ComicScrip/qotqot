2d59486dfed996f0fa6b270db649ddee93c4998e
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-06-03 11:19:38 +0200

    tried some specs

--- cypress/integration/nouvelleCommande.spec.js
+++ cypress/integration/nouvelleCommande.spec.js
@@ -4,4 +4,19 @@ describe("nouvelleCommande", () => {
   beforeEach(() => {
     cy.visit("/nouvelleCommande");
   });
+  it("should have content appear on screen", () => {
+    cy.get("div");
+  });
+  it("should present a picture of products", () => {
+    cy.get("img").should("be.visible");
+  });
+
+  it("should hace an id, name, weight and prices properties", () => {
+    cy.request({
+      method: "GET",
+      url: "http://localhost:3001/api/products",
+    }).then(function (response) {
+      expect(response.body[0].fields).contain("id");
+    });
+  });
 });

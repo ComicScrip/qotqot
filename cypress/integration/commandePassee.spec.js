4dac3ee9354176243e49cab4a793a9629d49a4d8
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-06-17 12:23:41 +0200

    started to write tests

--- cypress/integration/commandePassee.spec.js
+++ cypress/integration/commandePassee.spec.js
@@ -7,18 +7,21 @@ describe("commandePassee", () => {
   it("should access a detailed page of pending order when clicking on a pending order", () => {
     cy.visit("/commandes");
     cy.contains("12 mai").click();
+    cy.url().should("eq", "http://localhost:3000/commandes/ENT69-0013");
     cy.contains("Prévue pour le 12 mai 2022");
   });
 
   it("should access a detailed page of a passed order when clicking on a passed order", () => {
     cy.visit("/commandes");
     cy.contains("17 mai").click();
+    cy.url().should("eq", "http://localhost:3000/commandes/PRO69-0007");
     cy.contains("Livrée le 17 mai 2022");
   });
 
   it("should access a detailed page of cancelled order when clicking on a cancelled order", () => {
     cy.visit("/commandes");
     cy.contains("5 avr.").click();
+    cy.url().should("eq", "http://localhost:3000/commandes/PRO69-0006");
     cy.contains("Annulée le 5 avr. 2022");
   });
 });

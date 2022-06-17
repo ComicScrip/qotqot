0632df5e41e8bca773dad4896ed7a39a40d631e9
Félix <fb.felixberger@gmail.com>
2022-06-17 10:29:24 +0200

    integration account page

--- pages/api/customerCartItem/index.js
+++ pages/api/customerCartItem/index.js
@@ -3,13 +3,11 @@ import axios from "axios";
 import base from "../../../middlewares/common";
 import reqCurrentUser from "../../../middlewares/reqCurrentUser";
 
-async function getCustomerCartItems({ customer_id }) {
-  console.log(reqCurrentUser(customer_id));
+async function getCustomerCartItems({ req }) {
+  console.log(req.currentUser.customer_id);
   axios
     .get(
-      `https://api.airtable.com/v0/app5Yy06J0dhcG7Xb/Panier?filterByFormula=%7BCode_Client%7D%3D%22${reqCurrentUser(
-        customer_id
-      )}%22`,
+      `https://api.airtable.com/v0/app5Yy06J0dhcG7Xb/Panier?filterByFormula=%7BCode_Client%7D%3D%22${req.currentUser.customer_id}%22`,
       {
         headers: {
           Authorization: `Bearer ${process.env.AIR_TABLE_API_KEY}`,
--- pages/compte/index.js
+++ pages/compte/index.js
@@ -4,51 +4,81 @@ import Layout from "../../components/Layout";
 export default function Account() {
   return (
     <Layout pageTitle="nouvelle-commande">
-      <>
-        <form>
-          <div className={styles.company}>
-            <h1>Société</h1>
-            <label>Nom de l'établissement*</label>
-            <input></input>
-            <label>Raison sociale*</label>
-            <input></input>
-            <label>SIRET*</label>
-            <input></input>
-            <label>Numéro de TVA*</label>
-            <input></input>
+      <div className={styles.ctnAcc}>
+        <form className={styles.formAcc}>
+          <div className={styles.pAcc}>
+            <h1 className={styles.titleAcc}>Société</h1>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Nom de l'établissement*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Raison sociale*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>SIRET*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Numéro de TVA*</label>
+              <input></input>
+            </div>
           </div>
-          <div className={styles.adressBilling}>
-            <h1>Adresse de facturation</h1>
-            <label>Numéro et voie*</label>
-            <input></input>
-            <label>Code postal*</label>
-            <input></input>
-            <label>Ville*</label>
-            <input></input>
+          <div className={styles.pAcc}>
+            <h1 className={styles.titleAcc}>Adresse de facturation</h1>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Numéro et voie*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Code postal*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Ville*</label>
+              <input></input>
+            </div>
           </div>
-          <div className={styles.adressBilling}>
-            <h1>Adresse de livraison</h1>
-            <label>Numéro et voie*</label>
-            <input></input>
-            <label>Code postal*</label>
-            <input></input>
-            <label>Ville*</label>
-            <input></input>
+          <div className={styles.pAcc}>
+            <h1 className={styles.titleAcc}>Adresse de livraison</h1>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Numéro et voie*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Code postal*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Ville*</label>
+              <input></input>
+            </div>
           </div>
-          <div>
-            <h1>Contact</h1>
-            <label>Nom*</label>
-            <input></input>
-            <label>Prénom*</label>
-            <input></input>
-            <label>Téléphone*</label>
-            <input></input>
-            <label>Email*</label>
-            <input></input>
+          <div className={styles.pAcc}>
+            <h1 className={styles.titleAcc}>Contact</h1>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Nom*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Prénom*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Téléphone*</label>
+              <input></input>
+            </div>
+            <div className={styles.fieldsAcc}>
+              <label className={styles.labelAcc}>Email*</label>
+              <input></input>
+            </div>
           </div>
-          <button>Enregistrer</button>
+          <div className={styles.btnAcc}>
+            <button>Enregistrer</button>
+          </div>
         </form>
-      </>
+      </div>
     </Layout>
   );
 }

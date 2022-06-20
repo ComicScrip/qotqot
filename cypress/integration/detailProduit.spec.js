0d1a91743f07a63357d615a4ac5fa22c2fef31e2
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-06-20 17:16:22 +0200

    started implementing tests

---
+++ cypress/integration/detailProduit.spec.js
@@ -0,0 +1,11 @@
+describe("detailProduit", () => {
+  beforeEach(() => {
+    cy.viewport("iphone-6");
+    cy.login({ email: "user@gmail.com" });
+  });
+  it("should display details of the proper product", () => {
+    cy.intercept("**/products", { fixture: "products.json" });
+    cy.visit("/nouvelleCommande");
+    cy.get(".a").click();
+  });
+});

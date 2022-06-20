7bedcd01880530051e63137a73af640e317b9015
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-06-20 15:17:29 +0200

    working on popup appearance

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,47 +1,67 @@
 import style from "../styles/product_item.module.css";
-import Link from "next/link";
-//import Popup from "reactjs-popup";
+//import Link from "next/link";
+import { useState } from "react";
+import ProductDetail from "./ProductDetail";
 
 function ProductItem(props) {
-  const id = props.id;
+  //const id = props.id;
+  const [isDetailed, setIsDetailed] = useState(false);
   return (
-    <div className={style.item_wrapper}>
-      <div className={style.item_picture}>
-        <Link href={`/detailProduit/${id}`}>
+    <>
+      {isDetailed ? (
+        <ProductDetail
+          name={props.name}
+          weight={props.weight}
+          price={props.price}
+          pricePerKg={props.pricePerKg}
+          stock={props.stock}
+          picture={props.picture ? props.picture : ""}
+          makerPicture={props.makerPicture}
+          makerName={props.makerName}
+          makerAdress={props.makerAdress}
+          productDesc={props.productDesc}
+          makerDesc={props.makerDesc}
+          setIsDetailed={true}
+        />
+      ) : (
+        ""
+      )}
+      <div className={style.item_wrapper}>
+        <div className={style.item_picture} onClick={() => setIsDetailed(true)}>
           <img
             src={props.picture ? props.picture : "/images/notAvailable.png"}
             alt={props.name}
           />
-        </Link>
-      </div>
-      <div className={style.item_detail}>
-        <div className={style.item_title}>{props.name}</div>
-        <div className={style.item_weight}>{props.weight}</div>
-      </div>
-      <div className={style.price}>
-        <div className={style.itemPrice}>{props.price}€ HT</div>
-        <div className={style.itemPricePerKg}>{props.pricePerKg}€ HT /Kg</div>
-      </div>
-      <div className={style.item_stock}>
-        <div
-          className={
-            props.stock === "En stock"
-              ? style.stock_display_instock
-              : props.stock === "Stock faible"
-              ? style.low_on_stock
-              : style.out_of_stock
-          }
-        >
-          {props.stock}
         </div>
+        <div className={style.item_detail}>
+          <div className={style.item_title}>{props.name}</div>
+          <div className={style.item_weight}>{props.weight}</div>
+        </div>
+        <div className={style.price}>
+          <div className={style.itemPrice}>{props.price}€ HT</div>
+          <div className={style.itemPricePerKg}>{props.pricePerKg}€ HT /Kg</div>
+        </div>
+        <div className={style.item_stock}>
+          <div
+            className={
+              props.stock === "En stock"
+                ? style.stock_display_instock
+                : props.stock === "Stock faible"
+                ? style.low_on_stock
+                : style.out_of_stock
+            }
+          >
+            {props.stock}
+          </div>
 
-        <div className={style.counter}>
-          <div>-</div>
-          <div>Qt</div>
-          <div>+</div>
+          <div className={style.counter}>
+            <div>-</div>
+            <div>Qt</div>
+            <div>+</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 

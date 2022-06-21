5fbfdf9f15579162bbce2e9b254f7add2472bcc5
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-06-21 16:02:10 +0200

    finished tests

--- cypress/integration/commandePassee.spec.js
+++ cypress/integration/commandePassee.spec.js
@@ -2,13 +2,13 @@ describe("commandePassee", () => {
   beforeEach(() => {
     cy.viewport("iphone-6");
     cy.login({ email: "user@gmail.com" });
-    cy.intercept("**/commandes", { fixture: "orders.json" });
-    cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });
   });
 
   it("should access a detailed page of pending order when clicking on a pending order", () => {
     cy.request("https://qotqot.vercel.app/commandes");
+    cy.intercept("**/commandes", { fixture: "orders.json" });
     cy.contains("12 mai").click();
+    cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });
     cy.url().should("include", "/commandes/ENT69-0013");
     cy.contains("Prévue pour le 12/05/2022");
     cy.contains("Provençale de légumes");
@@ -16,7 +16,9 @@ describe("commandePassee", () => {
 
   it("should access a detailed page of a passed order when clicking on a passed order", () => {
     cy.request("https://qotqot.vercel.app/commandes");
+    cy.intercept("**/commandes", { fixture: "orders.json" });
     cy.contains("17 mai").click();
+    cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });
     cy.url().should("include", "/commandes/PRO69-0007");
     cy.contains("Livrée le 17/05/2022");
     cy.contains("Liqueur de Verveine");
@@ -24,7 +26,9 @@ describe("commandePassee", () => {
 
   it("should access a detailed page of cancelled order when clicking on a cancelled order", () => {
     cy.request("https://qotqot.vercel.app/commandes");
+    cy.intercept("**/commandes", { fixture: "orders.json" });
     cy.contains("5 avr.").click();
+    cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });
     cy.url().should("include", "/commandes/PRO69-0006");
     cy.contains("Annulée le 05/04/2022");
     cy.contains("Provençale de légumes");

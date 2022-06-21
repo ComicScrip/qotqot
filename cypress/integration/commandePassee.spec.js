baa2c85aab255b4cc33e80743468174319cc25b4
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-06-21 15:44:55 +0200

    finished tests

--- cypress/integration/commandePassee.spec.js
+++ cypress/integration/commandePassee.spec.js
@@ -7,7 +7,7 @@ describe("commandePassee", () => {
   });
 
   it("should access a detailed page of pending order when clicking on a pending order", () => {
-    cy.visit("/commandes");
+    cy.visit("**/commandes");
     cy.contains("12 mai").click();
     cy.url().should("include", "/commandes/ENT69-0013");
     cy.contains("Prévue pour le 12/05/2022");
@@ -15,7 +15,7 @@ describe("commandePassee", () => {
   });
 
   it("should access a detailed page of a passed order when clicking on a passed order", () => {
-    cy.visit("/commandes");
+    cy.visit("**/commandes");
     cy.contains("17 mai").click();
     cy.url().should("include", "/commandes/PRO69-0007");
     cy.contains("Livrée le 17/05/2022");
@@ -23,7 +23,7 @@ describe("commandePassee", () => {
   });
 
   it("should access a detailed page of cancelled order when clicking on a cancelled order", () => {
-    cy.visit("/commandes");
+    cy.visit("**/commandes");
     cy.contains("5 avr.").click();
     cy.url().should("include", "/commandes/PRO69-0006");
     cy.contains("Annulée le 05/04/2022");

fef89236dd3130cd470aeb4e18274f639166ddeb
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-06-22 15:27:06 +0200

    fixed testing

--- cypress/integration/detailProduit.spec.js
+++ cypress/integration/detailProduit.spec.js
@@ -9,17 +9,17 @@ describe("detailProduit", () => {
     cy.get("a").should("have.attr", "target").and("include", "_blank");
   });
   it("should display the correct properties of the clicked item", () => {
-    cy.visit("detailProduit/rec0cguxNDu8gyr0d");
+    cy.visit("/detailProduit/rec0cguxNDu8gyr0d");
     cy.contains("Douceur de quinoa coco carotte");
     cy.contains("CLAC!");
   });
   it("works with another product as well", () => {
-    cy.visit("detailProduit/rec0nwwI1HxKvN2SO");
+    cy.visit("/detailProduit/rec0nwwI1HxKvN2SO");
     cy.contains("Riz de Camargue IGP rond blanc «special risotto»");
     cy.contains("30800 - SAINT GILLES - GARD");
   });
   it("displays as message when there is no product description", () => {
-    cy.visit("detailProduit/rec0rp9lvCydHMM14");
+    cy.visit("/detailProduit/rec0rp9lvCydHMM14");
     cy.contains("Aucune desciption disponible, merci de nous contacter");
   });
 });

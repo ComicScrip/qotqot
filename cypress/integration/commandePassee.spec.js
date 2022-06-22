d4c2d13fec4fd00b1e79fde2741af929988ed25b
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-06-22 15:46:48 +0200

    fixing tests

--- cypress/integration/commandePassee.spec.js
+++ cypress/integration/commandePassee.spec.js
@@ -5,7 +5,7 @@ describe("commandePassee", () => {
   });
 
   it("should access a detailed page of pending order when clicking on a pending order", () => {
-    cy.intercept("**/api/commandes", { fixture: "orders.json" });
+    cy.intercept("**/api/orders", { fixture: "orders.json" });
     cy.visit("/commandes");
     cy.contains("12 mai").click();
     cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });
@@ -15,7 +15,7 @@ describe("commandePassee", () => {
   });
 
   it("should access a detailed page of a passed order when clicking on a passed order", () => {
-    cy.intercept("**/api/commandes", { fixture: "orders.json" });
+    cy.intercept("**/api/orders", { fixture: "orders.json" });
     cy.visit("/commandes");
     cy.contains("17 mai").click();
     cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });
@@ -25,7 +25,7 @@ describe("commandePassee", () => {
   });
 
   it("should access a detailed page of cancelled order when clicking on a cancelled order", () => {
-    cy.intercept("**/api/commandes", { fixture: "orders.json" });
+    cy.intercept("**/api/orders", { fixture: "orders.json" });
     cy.visit("/commandes");
     cy.contains("5 avr.").click();
     cy.intercept("**/commandePassee", { fixture: "commandePassee.json" });

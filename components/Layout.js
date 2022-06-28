973726c5f4234be3c625804d250c45fef3668c5b
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-06-28 10:00:18 +0200

    modified css

--- components/Layout.js
+++ components/Layout.js
@@ -20,7 +20,7 @@ export default function Layout({ children, pageTitle }) {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href={`https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap`}
+            href={`https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,400;0,500;1,300&display=swap`}
             rel="stylesheet"
           />
         </Head>

649fcaaadcb3f838708269b842be8439ed93b937
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-07-18 11:58:55 +0200

    modified routes to visualise only orders of one specific profile

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -4,13 +4,10 @@ import reqCurrentUser from "../../../middlewares/reqCurrentUser";
 import base from "../../../middlewares/common";
 
 export async function getAllProducts(req, res) {
+  const user = req.currentUser.fields["Code Client"];
   axios
     .get(
-      `${process.env.AIRTABLE_API}/Commandes%20Pro?filterByFormula=${
-        req.query.status === "pending"
-          ? `%7BStatus%7D%3D%22En-cours%22`
-          : `OR(%7BStatus%7D%3D%22Livr%C3%A9e%22%2C%7BStatus%7D%3D%22Annul%C3%A9e%22)`
-      }`,
+      `${process.env.AIRTABLE_API}/Commandes%20Pro%20Test?filterByFormula=%7BCode%20Client%7D%3D%22${user}%22`,
       {
         headers: {
           Authorization: `Bearer ${process.env.AIR_TABLE_API_KEY}`,
@@ -24,3 +21,5 @@ export async function getAllProducts(req, res) {
 }
 
 export default base().use(reqCurrentUser).get(getAllProducts);
+
+// Commandes%20Pro?filterByFormula=%7BCode%20Client%7D%3D%22${user}%22

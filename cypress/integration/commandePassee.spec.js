7cca9ddb51c543d94f5722648e78078d7163b178
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-07-26 17:38:56 +0200

    corrected bugs

--- cypress/integration/commandePassee.spec.js
+++ cypress/integration/commandePassee.spec.js
@@ -1,14 +1,14 @@
 describe("commandePassee", () => {
   beforeEach(() => {
     cy.viewport("iphone-6");
-    cy.login({ email: "user@gmail.com" });
+    cy.login({ email: "wcsqotqot@gmail.com" });
   });
 
   it("should access a detailed page of a passed order when clicking on a passed order", () => {
     cy.intercept("**/api/orders**", { fixture: "orderspassed.json" });
     cy.visit("/commandes");
     cy.contains("26 Jul").click();
-    cy.intercept("**/orderDetails/cl62b8ee3004609jw4t8pmb1a", {
+    cy.intercept("**/commandes/cl62b8ee3004609jw4t8pmb1a", {
       fixture: "orderDetails.json",
     });
     cy.url().should("include", "/commandes/cl62b8ee3004609jw4t8pmb1a");

2e6adc236e796731ecbd6bd2a589e8c343770f6f
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-07-27 09:52:38 +0200

    synced import from Airtable now upserts instead of delete+create

--- scripts/improvedImport.js
+++ scripts/improvedImport.js
@@ -7,14 +7,12 @@ const base = new Airtable({ apiKey: `${process.env.AIR_TABLE_API_KEY}` }).base(
 );
 
 export async function importProductsFromAT() {
-  //await db.Product.deleteMany();
-
   const records = await base("Produits Actifs").select().all();
   const products = minifyProducts(records);
   await Promise.all(
     products.map((item) => {
       return db.product.upsert({
-        where: {
+        create: {
           codeProduit: item.codeProduit,
           name: item.name,
           weight: item.weight,
@@ -33,6 +31,26 @@ export async function importProductsFromAT() {
           poidsUVC: item.poidsUVC,
           uniteUVC: item.uniteUVC,
         },
+        update: {
+          codeProduit: item.codeProduit,
+          name: item.name,
+          weight: item.weight,
+          price: item.price,
+          pricePerKg: item.pricePerKg,
+          stock: item.stock,
+          picture: item.picture,
+          makerPicture: item.makerPicture,
+          makerName: item.makerName,
+          makerAdress: item.makerAdress,
+          descriptionProduit: item.descriptionProduit,
+          descriptionProducteur: item.descriptionProducteur,
+          logo: item.logo,
+          category: item.category,
+          typeUVC: item.typeUVC,
+          poidsUVC: item.poidsUVC,
+          uniteUVC: item.uniteUVC,
+        },
+        where: { codeProduit: item.codeProduit },
       });
     })
   );
